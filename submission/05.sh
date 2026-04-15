d1f43604c314408dade760ef44fe9e3d1e4d5822
Bagwells <timmyayomidebagwells@outlook.com>
2026-04-15 14:12:35 +0100

    submission

--- submission/05.sh
+++ submission/05.sh
@@ -3,7 +3,7 @@
 publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 blocks=150
 hexblocks=$(printf '%x' "$blocks")
-lock_push="020096"
+lock_push="029600"
 publicKeyHash=$(echo -n "$publicKey" | xxd -r -p | openssl dgst -sha256 -binary | openssl dgst -rmd160 -binary | xxd -p -c 200)
 OP_CHECKSEQUENCEVERIFY="b2"
 OP_DROP="75"
@@ -13,7 +13,7 @@ PUSH20=14
 OP_EQUALVERIFY=88
 push33="21"
 OP_CHECKSIG="ac"
-script="${lock_push}${OP_CHECKSEQUENCEVERIFY}${OP_DROP}${OP_DUP}${OP_HASH160}${PUSH20}${pubKeyHash}${OP_EQUALVERIFY}${OP_CHECKSIG}"
+script="${lock_push}${OP_CHECKSEQUENCEVERIFY}${OP_DROP}${OP_DUP}${OP_HASH160}${PUSH20}${publicKeyHash}${OP_EQUALVERIFY}${OP_CHECKSIG}"
 
 echo $script
 
