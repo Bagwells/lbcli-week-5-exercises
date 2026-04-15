3210cdfa706874081656c21b8b94b5178436de6c
Bagwells <timmyayomidebagwells@outlook.com>
2026-04-15 13:34:41 +0100

    submission

--- submission/05.sh
+++ submission/05.sh
@@ -2,5 +2,17 @@
 
 publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 
-150 OP_CHECKSEQUENCEVERIFY OP_DROP $publicKey OP_CHECKSIG
+
+opcode=02
+lock_push=$(echo $block | sed -E 's/(..)(..)(..)(..)/\4\3\2\1/')
+OP_CHECKSEQUENCEVERIFY=b2
+OP_DROP=75
+push33=21
+OP_CHECKSIG=ac
+
+script="$lock_push$OP_CHECKSEQUENCEVERIFY$OP_DROP$push33$publicKey$OP_CHECKSIG"
+echo $script
+
+
+
 
