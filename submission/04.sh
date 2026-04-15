fd2879bec6a69550bb4f296cb77f6e2eb1433cb3
Bagwells <timmyayomidebagwells@outlook.com>
2026-04-15 13:23:00 +0100

    submission

--- submission/04.sh
+++ submission/04.sh
@@ -2,7 +2,18 @@
 # publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 
 publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+locktime=1495584032
+hex=(printf '%08x\n' $locktime)
+little_endian_result=$(echo $hex | sed -E 's/(..)(..)(..)(..)/\4\3\2\1/')
+publicKeyHash=$(echo -n "$publicKey" | xxd -r -p | sha256sum | ripemd160 | awk '{print $1}')
 
-SCRIPT=$(1495584032 OP_CHECKLOCKTIMEVERIFY OP_DROP $publicKey OP_CHECKSIG)
+OP_CHECKLOCKTIMEVERIFY=b1
+OP_DROP=75
+OP_DUP=76
+OP_HASH160=a9
+PUSH20=14
+OP_EQUALVERIFY=88
+OP_CHECKSIG=ac
 
-echo "$SCRIPT"
+script="04${little_endian_result}${OP_CHECKLOCKTIMEVERIFY}${OP_DROP}${OP_DUP}${PUSH20}${publicKeyHash}${OP_EQUALVERIFY}${OP_CHECKSIG}"
+echo $script
